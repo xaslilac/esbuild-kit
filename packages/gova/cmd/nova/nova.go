dda1ce6a3e05d54b49376ca09fd31916eca36401
McKayla Washburn <mckayla@hey.com>
2021-12-30 22:45:51 -0700

    Initial go based prototype

---
+++ packages/gova/cmd/nova/nova.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	esbuild "github.com/evanw/esbuild/pkg/api"
+)
+
+type NovaOptions struct {
+	NoCheck bool
+	Watch   bool
+}
+
+func alert(result esbuild.BuildResult) {
+	fmt.Println("Brt")
+	fmt.Println(result)
+}
+
+func main() {
+	var options NovaOptions
+
+	flag.BoolVar(&options.NoCheck, "noCheck", false, "Skips type checking")
+	flag.BoolVar(&options.Watch, "watch", false, "Watch files and recompile when changes are made")
+	flag.Parse()
+
+	fmt.Println("Options:", options)
+
+	// var watchMode *esbuild.WatchMode = nil
+	// if options.Watch {
+	// 	watchMode = &esbuild.WatchMode{OnRebuild: alert}
+	// }
+
+	var watchMode *esbuild.WatchMode = &esbuild.WatchMode{OnRebuild: alert}
+
+	fmt.Println(watchMode)
+
+	result := esbuild.Build(esbuild.BuildOptions{
+		EntryPoints: []string{"./main.ts"},
+		Bundle:      true,
+		Format:      esbuild.FormatESModule,
+		JSXMode:     esbuild.JSXModePreserve,
+		Outbase:     "./",
+		Outdir:      "./target/",
+		Watch:       watchMode,
+		Write:       true,
+	})
+
+	if len(result.Errors) > 0 {
+		fmt.Println("err")
+	}
+}
---
+++ packages/gova/go.mod
@@ -0,0 +1,7 @@
+module github.com/aslilac/nova
+
+go 1.17
+
+require github.com/evanw/esbuild v0.14.9
+
+require golang.org/x/sys v0.0.0-20210908233432-aa78b53d3365 // indirect
